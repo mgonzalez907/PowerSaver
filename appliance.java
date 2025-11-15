0a59ca172e0feffc417b0a5afa8adc0f6a7ff014
mgonzalez907 <mgonzalez907@toromail.csudh.edu>
2025-11-15 03:06:38 -0800

    Add files via upload

---
+++ appliance.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+//appliance setting up variables it works with 
+public class appliance{
+    private String name;
+    private double watts;
+    private double volts;
+    private double current;
+    private double cost;
+    
+    //constructor to create appliances (Setters)
+    public appliance(String name, double watts, double volts, double current, double cost){
+        this.name = name;
+        this.watts = watts;
+        this.volts = volts;
+        this.current = current;
+        this.cost = cost;
+    }
+
+    //Getter methods
+    public String getName(){
+        return name;
+    }
+
+    public double getWatts(){
+        return watts;
+    }
+
+    public double getVolts(){
+        return volts;
+    }
+
+    public double getCurrent(){
+        return current;
+    }
+
+    public double getCost(){
+        return cost;
+    }
+
+
+
+    
+
+        
+
+    public static void main(String[] args){
+        int DAYS_TO_HOURS = 24;
+        int WEEK_TO_DAYS = 7;
+        int MONTH_TO_DAYS = 30;
+        int YEAR_TO_MONTHS = 12;
+        double HOURLY_RATE = 0.0;
+
+        
+        Scanner keyboard = new Scanner(System.in);
+        ArrayList<appliance> allAppliances = new ArrayList<>();
+        System.out.printf("HELLO WELCOME TO POWER SAVER\n");
+        System.out.printf("What is the Hourly Rate per kws in your state:\n $");
+        if(keyboard.hasNextDouble()){
+            HOURLY_RATE = keyboard.nextDouble();
+        }
+        else{
+            System.out.printf("Error:\nThe input give is not a valid currency(USD): EXAMPLE 1.23\n");
+        }
+
+        System.out.println("Enter the name of an appliance you wish to track");
+        String name = keyboard.nextLine();
+
+    }
+}
